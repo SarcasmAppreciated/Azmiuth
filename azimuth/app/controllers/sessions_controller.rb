3dc4ffc0d792da5e41de16cf255484e2d5d7ea2b
alimv <alimv@cs.ubc.ca>
2015-02-13 05:54:54 -0800

    I make terrible life decisions

--- azimuth/app/controllers/sessions_controller.rb
+++ azimuth/app/controllers/sessions_controller.rb
@@ -1,7 +1,5 @@
 class SessionsController < ApplicationController
-  def new
-  endrender :text => auth_hash.inspect
-  end
+  
 
   def create
 
@@ -9,17 +7,24 @@ class SessionsController < ApplicationController
 
 	@authorization = Authorization.find_by_provider_and_uid("twitter", auth_hash["uid"])
 	if @authorization
+		@user = @authorization.user
 		render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
 	else
 		#user = User.new :name => auth_hash["user_info"]["name"], :email => auth_hash["user_info"]["email"]
 		#user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
 		#user.save
 
-		user = User.new :name => auth_hash["info"]["name"], :email =>  "default@mail.com"
-		user.authorizations.build :provider => "twitter", :uid => auth_hash["uid"]
-		user.save
+		@user = User.new :name => auth_hash["info"]["name"], :email =>  "default@mail.com"
+		@user.password = "password"
 
-		render :text => "Hi #{user.name}! You've signed up."
+		@user.authorizations.build :provider => "twitter", :uid => auth_hash["uid"]
+		@user.save
+		@user.errors.each do |error|
+			puts error
+		end
+
+
+		render :text => "Hi #{@user.name}! You've signed up."
 	end
   end
 
