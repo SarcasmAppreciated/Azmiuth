1c5b325304f12aee40b32af6c56530c75ba6929f
alimv <alimv@cs.ubc.ca>
2015-02-13 13:42:27 -0800

    got rid of poitnless route

--- azimuth/config/routes.rb
+++ azimuth/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get   '/login', :to => 'sessions#new', :as => :login
+  #get   '/login', :to => 'sessions#new', :as => :login
   get '/auth/twitter/callback', :to => 'sessions#create'
   delete '/auth/failure', :to => 'sessions#failure'
   get '/logout', :to => 'sessions#destroy'
