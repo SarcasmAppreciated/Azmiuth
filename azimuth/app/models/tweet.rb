b93a25a49f311d1095a255dd1aa0f97e968ee3b2
alimv <alimv@cs.ubc.ca>
2015-03-06 01:03:50 -0800

    verify that all geo data is valid

--- azimuth/app/models/tweet.rb
+++ azimuth/app/models/tweet.rb
@@ -1,5 +1,7 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
-  validates :tweet_id, :time_stamp, :longitude,  :presence => true 
+  validates :tweet_id, :time_stamp, :latitude, :longitude,  :presence => true 
   validates :tweet_id, uniqueness: true
+  validates :latitude, :inclusion => -90..90
+  validates :longitude, :inclusion => -180..180
 end
