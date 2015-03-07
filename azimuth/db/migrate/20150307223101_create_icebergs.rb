a05805979f24ebf41b93ec84cab1413d34a744ad
Paul Cernek <ptcernek@gmail.com>
2015-03-07 14:39:14 -0800

    Remove some extraneous fields from iceberg database object

--- azimuth/db/migrate/20150307223101_create_icebergs.rb
+++ azimuth/db/migrate/20150307223101_create_icebergs.rb
@@ -1,15 +1,12 @@
 class CreateIcebergs < ActiveRecord::Migration
   def change
     create_table :icebergs do |t|
-      t.integer :ice_year
       t.integer :berg_number
       t.datetime :time
       t.float :latitude
       t.float :longitude
-      t.string :method
       t.string :size
       t.string :shape
-      t.string :source
 
       t.timestamps null: false
     end
