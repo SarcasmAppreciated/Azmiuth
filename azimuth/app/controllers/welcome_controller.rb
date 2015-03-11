b7a7a397d931715efa109cf69d4e19a298e09792
alimv <alimv@cs.ubc.ca>
2015-03-10 18:52:41 -0700

    Allowed the tweets of a current user to be accessed from welcome index

--- azimuth/app/controllers/welcome_controller.rb
+++ azimuth/app/controllers/welcome_controller.rb
@@ -3,6 +3,11 @@ class WelcomeController < ApplicationController
     @user = current_user
     month_day = Date.today.strftime("%d-%m")
     @icebergs = Iceberg.where("strftime('%d-%m', date) = ?", month_day)
+    if user_signed_in
+      @user_tweets = @user.tweets(:sort){:time_stamp}
+    else
+      @user_tweets = nil
+    end
   end
 
   def user_signed_in
