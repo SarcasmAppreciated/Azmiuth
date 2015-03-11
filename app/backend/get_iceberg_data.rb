a6167d261ccd778c577c60973a45d1fd907aeb94
Paul Cernek <ptcernek@gmail.com>
2015-03-10 22:32:13 -0700

    More troubleshooting

--- app/backend/get_iceberg_data.rb
+++ app/backend/get_iceberg_data.rb
@@ -1,3 +1,4 @@
+=begin
 require 'net/ftp'
 
 iceberg_data = "IIP_2014IcebergSeason.csv"
@@ -6,3 +7,4 @@ ftp = Net::FTP.new("sidads.colorado.edu")
 ftp.login
 ftp.chdir("pub/DATASETS/NOAA/G00807/")
 ftp.getbinaryfile(iceberg_data, Rails.root.join("db", iceberg_data).to_s)
+=end
