74a8f2ddb5f10e618a3ec57d2d6ac8d40e9b9b66
alimv <alimv@cs.ubc.ca>
2015-03-19 05:47:06 -0700

    Cleanup

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,16 +4,35 @@ class Tweet < ActiveRecord::Base
 	validates :tweet_id, uniqueness: true
 	validates :latitude, :inclusion => -90..90
 	validates :longitude, :inclusion => -180..180
-
-
 end
+
+@@number_of_tweets_to_pull = 25
 
 public
 	def update_all_users_aux
 		User.all.each do |user|
-			update_user_tweets(user, 25)
+			update_user_tweets(user, @@number_of_tweets_to_pull)
 		end
 	end 
+
+	def pushTweet(user, message)
+        client = get_client(user)
+        client.update(message)
+    end
+
+    #useless method meant to seed database
+    def push_tweet_with_geo(user, message, input_lat, input_long)
+        client = get_client(user)
+        client.update(message, {:lat=> input_lat, :long=> input_long})
+    end
+
+   	def update_user_tweets(user, n)
+		responses = pull_last_n_tweets(user,n)
+		responses.each do |response|
+			process_tweet(response, user)
+		end
+	end
+
 private 
 	def get_client(user)
 		auth = user.authorization
@@ -25,14 +44,6 @@ private
 		end
 		return client 
 	end
-
-	def update_user_tweets(user, n)
-		responses = pull_last_n_tweets(user,n)
-		responses.each do |response|
-			process_tweet(response, user)
-		end
-	end
-
 
 	def pull_last_n_tweets(user, n)
 		client = get_client(user)
@@ -47,24 +58,24 @@ private
 		geo = response.geo
 		if geo.nil?
             #will not process a tweet without a geoTag
-            #flash.now[:notice] = "no geotag for #{id}!"
+            puts "no geotag for #{id}!"
             return
         end
         coordinates = geo.coordinates
         if coordinates.nil?
             #will not process a tweet without a corrdinate
-            #flash.now[:notice] = "no coordinates for #{id}!"
+            puts "no coordinates for #{id}!"
             return
         end
         lat = coordinates.first
         long = coordinates.second
         if lat.nil? or long.nil?
              #will not process a tweet without a lat/long
-            #flash.now[:notice] = "no lat/long for #{id}!"
+            puts "no lat/long for #{id}!"
             return 
         end
         user.tweets.build :tweet_text => tweet_text, :time_stamp => time_stamp, :latitude => lat, :longitude => long, :tweet_id => id
         user.save
         puts tweet_text
-        #flash.now[:notice] = "processed tweet #{id}!"
+        puts "processed tweet #{id}!"
     end
