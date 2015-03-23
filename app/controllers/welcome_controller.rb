ca3ea5a27d8a42b9952bf291450d6fb17aec69c4
Anushka Agrawal <agrawalanushka93@gmail.com>
2015-03-23 01:09:57 +0000

    method to share signed in user's public page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 require 'time'
 
 class WelcomeController < ApplicationController
+  helper_method :share_my_path
+  
   def index
     @user = current_user
     month = Date.today.strftime("%m")
@@ -19,6 +21,19 @@ class WelcomeController < ApplicationController
   	else 
   		return true
   	end
+  end
+  
+  def share_my_path
+     @user = current_user
+    if @user 
+      shareable_url = request.base_url+ "/users/" + @user.user_id.to_s
+      twitter_share_message = "Look at my boat go! #AzimuthSailing " + shareable_url
+       r = rand(1000)
+      twitter_share_message += " " + r.to_s
+      Tweet.pushTweet(@user, twitter_share_message)
+    else 
+      raise "User not signed in"
+    end 
   end
   
 end
